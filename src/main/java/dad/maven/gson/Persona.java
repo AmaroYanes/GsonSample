b967cc767ffde2dbddaea6ced86db753e997b015
AmaroYanes <ihvamaroyanes@gmail.com>
2019-09-20 10:22:25 +0100

    Actualizacion codigo

--- src/main/java/dad/maven/gson/Persona.java
+++ src/main/java/dad/maven/gson/Persona.java
@@ -1,9 +1,9 @@
 package dad.maven.gson;
 
 public class Persona {
-	String nombre;
-	String apellidos;
-	int edad;
+	private String nombre;
+	private String apellidos;
+	private int edad;
 	
 	public Persona(String nombre, String apellido, int edad) {
 		this.nombre = nombre;
@@ -12,7 +12,5 @@ public class Persona {
 	}
 	public String toString() {
 		return "nombre: "+nombre+"\napellido: "+apellidos+"\nedad: "+ edad;
-		
 	}
-
 }
